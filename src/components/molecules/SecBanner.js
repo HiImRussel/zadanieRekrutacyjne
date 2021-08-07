c0426104229143570db13dd5ba66353d13d09c58
Russel <russel1234561@gmail.com>
2021-08-07 14:55:38 +0200

    Added changes in line-height properties

--- src/components/molecules/SecBanner.js
+++ src/components/molecules/SecBanner.js
@@ -3,9 +3,13 @@ import "../../css/secBanner.css";
 const SecBanner = () => {
   return (
     <section className="secBanner">
-      <h1>Skorzystaj z najszybciej rozwijającej się gałęzi medycyny.</h1>
+      <h1>
+        Skorzystaj z najszybciej
+        <br /> rozwijającej się gałęzi medycyny.
+      </h1>
       <p>
         Teleporada lekarska na naszym serwisie oznacza potwierdzoną skuteczność
+        <br />
         terapeutyczną, oszczędność czasu i pieniędzy.
       </p>
     </section>
