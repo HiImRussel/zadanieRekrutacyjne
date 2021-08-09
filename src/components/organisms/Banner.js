acab48baaee50ed4b8da6a432875b702b60fb2ad
Russel <russel1234561@gmail.com>
2021-08-09 14:54:33 +0200

    Added secbanner-section animation

--- src/components/organisms/Banner.js
+++ src/components/organisms/Banner.js
@@ -1,6 +1,41 @@
 import SecBanner from "../molecules/SecBanner";
 
+import gsap from "gsap/gsap-core";
+import { CSSPlugin } from "gsap/CSSPlugin";
+import { useEffect } from "react";
+import $ from "jquery";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerPlugin(CSSPlugin);
+
 const Banner = () => {
+  useEffect(() => {
+    const secBanner = $(".secBanner").children().toArray();
+    let delay = 0;
+
+    secBanner.forEach((element) => {
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: ".secBanner",
+            start: "-20% top",
+          },
+        })
+        .fromTo(
+          element,
+          {
+            transform: "translateY(20px)",
+            opacity: 0,
+            ease: "Power4.In",
+          },
+          { transform: "translateY(0px)", opacity: 1 }
+        )
+        .delay(delay);
+      delay += 1;
+    });
+  }, []);
   return <SecBanner />;
 };
 
--- src/components/organisms/FAQ.js
+++ src/components/organisms/FAQ.js
@@ -3,6 +3,15 @@ import FaqLeft from "../molecules/FaqLeft";
 import "../../css/faq.css";
 import FaqBoxes from "./FaqBoxes";
 
+import gsap from "gsap/gsap-core";
+import { CSSPlugin } from "gsap/CSSPlugin";
+import { useEffect } from "react";
+import $ from "jquery";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(CSSPlugin);
+
 const FAQ = ({ faqBoxesData }) => {
   return (
     <section className="faq">
